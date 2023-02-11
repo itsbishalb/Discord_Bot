9ab38e91a28dd10751d34c7fddc4d32a7fc17571
Bishal Bhandari <psybb7@nottingham.ac.uk>
2023-02-11 23:36:45 +0000

    date check seperate function

--- commands/kick.js
+++ commands/kick.js
@@ -23,33 +23,11 @@ module.exports = {
 			duration: interaction.options.getString('duration'),
 
 		};
+        
+		let output = await validateDate(date);
 
-		if (!checkHour(date.startingtime)) {
-			return interaction.reply("Invalid Start Time Please enter hour in (HH:MM) format like 23:00 for 11 PM")
-		} else if (!checkHour(date.duration)) {
-			return interaction.reply("Invalid Start Time Please enter hour in (HH:MM) format like 1:00 for 1 hour")
-		}
-		if (date.month < 1 || date.month > 12) {
-			return interaction.reply('Month invalid');
-		}
-		if (date.day < 1 || date.day > 31) {
-			return interaction.reply('Day invalid');
-		}
-		let wantedDate;
-		try {
-			wantedDate = new Date(date.year, date.month - 1 , date.day)
-		} catch (err) {
-			return interaction.reply('Invalid Date');
-		}
-
-		if (!(wantedDate.getFullYear() ==  date.year && wantedDate.getMonth() ==  (date.month-1) && wantedDate.getDate() == date.day)) {
-			return interaction.reply('Invalid Date: Please check date');
-		}
-		if (await weekDiff(wantedDate) > 2 ) {
-			console.log(await weekDiff(wantedDate))
-			return interaction.reply('Please only book two weeks in advance!');
-		}else if(await weekDiff(wantedDate) < 0){
-			return interaction.reply('Ofcourse, Time travel does not exist. Please only book for dates in future');
+		if(output = ''){
+		return interaction.reply(output)
 		}
 
 		// // var member = interaction.options.getString('date');
@@ -68,3 +46,39 @@ async function weekDiff(bookedDate) {
 	weeks = Math.floor((bookedDate - currentDate) / (7 * 24 * 60 * 60 * 1000));
 	return weeks;
 }
+
+
+
+async function validateDate(date) {
+	const startTime = date.startingtime.split(':');
+	const hour = parseInt(startTime[0], 10);
+	const minute = parseInt(startTime[1], 10);
+	if (isNaN(hour) || hour < 0 || hour > 23) {
+	  return 'Invalid Start Time. Please enter hour in (HH:MM) format, like 23:00 for 11 PM';
+	}
+	if (isNaN(minute) || minute < 0 || minute > 59) {
+	  return 'Invalid Start Time. Please enter minute in (HH:MM) format, like 23:00 for 11 PM';
+	}
+	const duration = parseInt(date.duration, 10);
+	if (isNaN(duration) || duration < 0) {
+	  return 'Invalid Duration. Please enter duration in (HH:MM) format, like 1:00 for 1 hour';
+	}
+	if (date.month < 1 || date.month > 12) {
+	  return 'Month invalid';
+	}
+	if (date.day < 1 || date.day > 31) {
+	  return 'Day invalid';
+	}
+	const wantedDate = new Date(date.year, date.month - 1, date.day);
+	if (isNaN(wantedDate.getTime())) {
+	  return 'Invalid Date';
+	}
+	const daysDifference = weekDiff(wantedDate);
+	if (daysDifference > 14) {
+	  return 'Please only book two weeks in advance';
+	} else if (daysDifference < 0) {
+	  return 'Time travel does not exist. Please only book for dates in the future';
+	}
+	return '';
+  }
+  
